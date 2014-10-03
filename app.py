a6da7703151413ff05efe2cd1d775a70c50eefb4
aviau <alexandre.viau@savoirfairelinux.com>
2014-10-02 20:50:17 -0400

    First commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask.ext import restful
+
+import wsme
+from wsme import types as wtypes
+from wsmeext.flask import signature
+
+
+app = Flask(__name__)
+api = restful.Api(app)
+
+class Info(wsme.types.Base):
+    message = wsme.types.text
+
+class HelloWorld(restful.Resource):
+
+    @signature(Info)
+    def get():
+        return Info(message="hello")
+
+    @signature(None, Info)
+    def post(info):
+        print("Got a message: %s" % info.message)
+
+api.add_resource(HelloWorld, '/hello')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ test_app.py
@@ -0,0 +1,23 @@
+import unittest
+import app
+import json
+
+class FunctionalTest(unittest.TestCase):
+
+    def setUp(self):
+        app.app.config['TESTING'] = True
+        self.app = app.app.test_client()
+
+    def test_get(self):
+        response = self.app.get('/hello')
+        self.assertEqual(
+            json.loads(response.data.decode()),
+            {"message": "hello"}
+        )
+
+    def test_post(self):
+        response = self.app.post('/hello')
+        self.assertEqual(
+            200,
+            response.status_code
+        )
